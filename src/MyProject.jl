ec6cffcbf88be157d57f05482037f907c99f77bc
Daisuke1988 <daisuke.nakamura.kuhachi.felix@gmail.com>
2024-04-05 21:02:56 +0900

    update 240405-1

--- src/MyProject.jl
+++ src/MyProject.jl
@@ -3,6 +3,7 @@ module MyProject
 using Expectations, Distributions
 
 function foo(μ = 1., σ = 2.)
+    println("Modified foo definition")
     d = Normal(μ, σ)
     E = expectation(d)
     return E(x -> sin(x))
